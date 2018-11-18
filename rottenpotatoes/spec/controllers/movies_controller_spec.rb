0fcaf1c9d3860d839fa4012dd1c3c448e720aaf7
Alvaro Sainz-Pardo <alvarosainzpardo@gmail.com>
2018-11-18 19:44:03 +0100

    hw5-part2: better coverage

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -2,12 +2,36 @@ require 'rails_helper'
 
 describe MoviesController do
 
-  it 'should have a "same director" method with :id param' do
-    post :same_director, {id: 1}
+  before do
+    movie1 = instance_double(Movie, title: "Alien", director: "Ridley Scott")
+    allow(Movie).to receive(:find).and_return(movie1)
   end
 
-  it 'should call the "search_same_director" model method with :id param' do
-    expect(Movie).to receive(:find_same_director).with('1')
-    post :same_director, {id: 1}
+  it 'should have a "same director" method with :id param' do
+    get :same_director, {id: 1}
+  end
+
+  context 'movie has a director' do
+    before do
+      movie2 = instance_double(Movie, title: "Alien", director: "Ridley Scott")
+      allow(Movie).to receive(:find).and_return(movie2)
+    end
+
+    it 'should call the "find_same_director" model method with :id param' do
+      expect(Movie).to receive(:find_same_director)
+      get :same_director, {id: 1}
+    end
+  end
+
+  context 'movie has nor a director' do
+    before do
+      movie = instance_double(Movie, title: "Alien", director: "")
+      allow(Movie).to receive(:find).and_return(movie)
+    end
+
+    it 'should not call the "find_same_director" model method with :id param' do
+      expect(Movie).not_to receive(:find_same_director)
+      get :same_director, {id: 1}
+    end
   end
 end
